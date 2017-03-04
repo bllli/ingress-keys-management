fb5d03a7647360c334cf9bc868f4fa65b8f416ac
bllli <me@lidengming.com>
2017-03-04 17:49:35 +0800

    add portal信息修改/ 修改portal添加逻辑及提示

--- app/portal/views.py
+++ app/portal/views.py
@@ -22,22 +22,40 @@ def portal_info(po_id):
 def add():
     form = AddPortalForm()
     if form.validate_on_submit():
-        old_po = Portal.query.filter_by(link=form.link.data).first()
-        if old_po is not None:
-            flash('该portal已经被添加!<a href="%s">点击查看</a>' % url_for('portal.portal_info', po_id=old_po.id, _external=True))
-            return redirect(url_for('portal.add'))
+        if form.link.data is not None:
+            old_po = Portal.query.filter_by(link=form.link.data).first()
+            if old_po is not None:
+                flash('portal link重复,添加失败!<a href="%s">点击查看重复的po</a>'
+                      % url_for('portal.portal_info', po_id=old_po.id, _external=True))
+                return redirect(url_for('portal.add'))
         po = Portal(name=form.name.data, area=form.area.data,
                     link=form.link.data, submitter_id=current_user.id)
         db.session.add(po)
-        flash('添加成功, Thank you~')
+        flash('添加成功, Thank you~<a href="%s">点击查看新添加的po</a>'
+              % url_for('portal.portal_info', po_id=po.id, _external=True))
         return redirect(url_for('portal.add'))
     return render_template('portal/add.html', form=form)
 
 
-@portal.route('/modify/<po_id>')
-@permission_required(Permission.MODIFY_PORTAL)
+@portal.route('/modify/<po_id>', methods=['GET', 'POST'])
+# @permission_required(Permission.MODIFY_PORTAL)
 def modify(po_id):
-    pass
+    po = Portal.query.get_or_404(po_id)
+    if current_user.can(Permission.MODIFY_PORTAL) or po.submitter_id == current_user.id:
+        form = AddPortalForm()
+        if form.validate_on_submit():
+            po.name = form.name.data
+            po.link = form.link.data
+            po.area = form.area.data
+            db.session.add(po)
+            return redirect(url_for('portal.portal_info', po_id=po_id))
+        form.name.data = po.name
+        form.link.data = po.link
+        form.area.data = po.area
+        return render_template('portal/modify.html', form=form, portals=[po])
+    else:
+        flash('抱歉, 只有po提交者和管理员才能修改po信息.')
+        return redirect(url_for('portal.portal_info', po_id=po_id))
 
 
 @portal.route('/change_amount/<po_id>')
