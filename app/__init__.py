c3700e8148de5134cce25902b6277942c25d17dd
bllli <me@lidengming.com>
2017-03-09 10:11:41 +0800

    挖坑 下面要做 用户集合/用户管理/portal导入功能

--- app/__init__.py
+++ app/__init__.py
@@ -71,4 +71,7 @@ def create_app(config_name):
     from .wechat import keybot as wechat_blueprint
     app.register_blueprint(wechat_blueprint, url_prefix='/wechat')
 
+    from .admin import admin as admin_blueprint
+    app.register_blueprint(admin_blueprint, url_prefix='/admin')
+
     return app
---
+++ app/admin/views.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from flask import render_template, redirect, url_for, abort, flash, request, jsonify
+from flask_login import login_required, current_user
+from .. import db
+from ..models import Role, User, Permission, Portal, Have
+from ..decorators import permission_required, admin_required
+
+from . import admin
+
+
+@admin.route('/')
+@login_required
+@admin_required
+def admin():
+    pass
+
+
+@admin.route('/agent_management')
+@login_required
+@admin_required
+def agent_manage():
+    pass
+
+
+@admin.route('/insert_portals')
+@login_required
+@admin_required
+def insert_portals():
+    pass
+
+
+@admin.route('/agent_set')
+@login_required
+@admin_required
+def agent_set():
+    pass
