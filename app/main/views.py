0582ce45f50b14b12259ab41e3210fc1568859b5
bllli <me@lidengming.com>
2017-03-10 20:24:18 +0800

    整理一下视图

--- app/main/views.py
+++ app/main/views.py
@@ -11,7 +11,7 @@ from ..decorators import admin_required
 @login_required
 def index():
     page = request.args.get('page', 1, type=int)
-    pagination = Portal.query.order_by(Portal.timestamp.desc()).paginate(page, per_page=50, error_out=False)
+    pagination = Portal.query.order_by(Portal.timestamp.asc()).paginate(page, per_page=50, error_out=False)
     portals = pagination.items  # enumerate(pagination.items)
     return render_template('index.html', page=page, portals=portals, pagination=pagination)
 
--- app/portal/forms.py
+++ app/portal/forms.py
@@ -9,6 +9,6 @@ from ..models import Role, User
 
 class AddPortalForm(FlaskForm):
     name = StringField('po名', validators=[DataRequired(), Length(0, 128)])
-    area = StringField('所处区域', validators=[DataRequired(), Length(0,128)])
+    area = StringField('所处区域', validators=[Length(0,128)])
     link = StringField('PORTAL link', validators=[Length(0, 128)])
     submit = SubmitField('提交')
