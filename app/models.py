edfa3c352aa72ed61e8bb4b1dbe5c210731edbcc
bllli <me@lidengming.com>
2017-03-10 19:17:02 +0800

    配置邮箱

--- app/models.py
+++ app/models.py
@@ -105,7 +105,7 @@ class User(UserMixin, db.Model):
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
         if self.role is None:
-            if self.email == current_app.config['FLASKY_ADMIN']:
+            if self.email == current_app.config['ENL_ADMIN']:
                 self.role = Role.query.filter_by(permissions=0xff).first()
             if self.role is None:
                 self.role = Role.query.filter_by(default=True).first()
--- config.py
+++ config.py
@@ -1,19 +1,24 @@
+# -*- coding: utf-8 -*-
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SECRET_KEY = os.getenv('SECRET_KEY') or '2B|!2B, there is a question'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    # Email stuff
+    MAIL_SERVER = 'smtp.qq.com'
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'your email addr'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'your email passwd'
+    # 邮箱密码是QQ邮箱的授权码, 需要在QQ邮箱单独申请
+
+    # administrator's email account
+    ENL_MAIL_SUBJECT_PREFIX = '[ENL KEY]'
+    ENL_MAIL_SENDER = MAIL_USERNAME + '@qq.com'
+    ENL_ADMIN = os.environ.get('ENL_ADMIN') or 'your administrator email account'
 
     @staticmethod
     def init_app(app):
